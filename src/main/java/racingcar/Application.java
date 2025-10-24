109799bc606bcd92b039cd9e0920b4b446945ca6
ndwPC <magma0409@gmail.com>
2025-10-24 17:24:40 +0900

    feat: 자동차 이름 분리와 시도할 횟수 정수화하기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,6 +8,9 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         System.out.println("시도할 횟수는 몇 회인가요?");
-        String nums = Console.readLine();
+        String numstr = Console.readLine();
+
+        String[] cars = input.split(",");
+        int num = Integer.parseInt(numstr);
     }
 }
