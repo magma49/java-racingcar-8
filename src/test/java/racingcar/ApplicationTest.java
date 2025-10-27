c141b905fe9740b916ff258eaa45aabe80ba37b1
ndwPC <magma0409@gmail.com>
2025-10-27 17:37:02 +0900

    feat: Test 추가하기

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -13,7 +13,7 @@ class ApplicationTest extends NsTest {
     private static final int STOP = 3;
 
     @Test
-    void 기능_테스트() {
+    void 기능_테스트1() {
         assertRandomNumberInRangeTest(
             () -> {
                 run("pobi,woni", "1");
@@ -24,10 +24,39 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 기능_테스트2() {
+        assertSimpleTest(
+                () -> {
+                    run("pobi,woni,jun", "5");
+                    assertThat(output()).contains("pobi : -", "woni : -","jun : -", "최종 우승자 :");
+                }
+        );
+    }
+
+    @Test
+    void 기능_테스트3() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,jun", "2");
+                    assertThat(output()).contains("pobi : -", "woni : ","jun : ", "최종 우승자 : pobi,jun");
+                },
+                7,1,3,2,2,4,8,6,5
+        );
+    }
+
+    @Test
+    void 예외_테스트1() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,magma", "0"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
